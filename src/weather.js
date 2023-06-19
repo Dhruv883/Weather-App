767a59b3e430a930116c888967d1b8a111cbdb16
Dhruv <dhruvdedhia03@gmail.com>
2023-06-19 22:28:49 +0530

    Dotenv

--- src/weather.js
+++ src/weather.js
@@ -1,29 +1,33 @@
-let error = document.getElementById('error')
-let search = document.getElementById('search')
+let error = document.getElementById("error");
+let search = document.getElementById("search");
+require("dotenv").config();
+
+const apiid = process.env.API_KEY;
 
 async function weather(city) {
-      try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=137c4ebd53c2e7690e3075aaaf98290b`, { mode: 'cors' })
-            const obj = await response.json();
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiid}`,
+      { mode: "cors" }
+    );
+    const obj = await response.json();
 
-            const data = {
-                  city: obj.name,
-                  country: obj.sys.country,
-                  main: obj.main,
-                  wind: obj.wind.speed,
-                  weather: obj.weather[0].main
-            }
+    const data = {
+      city: obj.name,
+      country: obj.sys.country,
+      main: obj.main,
+      wind: obj.wind.speed,
+      weather: obj.weather[0].main,
+    };
 
-            return data;
-
-      } catch (err) {
-            error.setAttribute('style', 'display:block')
-            setTimeout(() => {
-                  error.setAttribute('style', 'display:none')
-                  search.value = ''
-
-            }, 3500)
-      }
+    return data;
+  } catch (err) {
+    error.setAttribute("style", "display:block");
+    setTimeout(() => {
+      error.setAttribute("style", "display:none");
+      search.value = "";
+    }, 3500);
+  }
 }
 
 export default weather;
